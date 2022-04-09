a536fc9d74a102364093d3e3fe7ae3a5be7a54bb
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-04-09 14:41:19 +0530

    completed

---
+++ Lab/Practical/Question11.java
@@ -0,0 +1,37 @@
+
+
+class Shape{
+    
+    float area;
+
+    void area(int length , int breadth)
+    {
+        area = length * breadth; // area of rectangle
+    }
+
+    void area(float half , int base , int height)
+    {
+        area =  half * base * height; // area of triangle
+    }
+
+    void area(int radius)
+    {
+        area = 3.14 * (int)radius * (int)radius; // area of circle
+    }
+
+    void display(){
+        System.out.print("Area is = " + area);
+    }
+
+
+    public static void main(String args[])
+    {
+        Shape s = new Shape();
+        s.area(2 , 4);
+        s.display();
+        s.area(0.2f , 4 , 6);
+        s.display();
+        s.area(4);
+        s.display();
+    }
+}

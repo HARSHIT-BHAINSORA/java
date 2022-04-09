442e95b01ff54338035016a5391cb1ecabf7ab17
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-04-09 21:01:07 +0530

    Overloading

--- Lab/Practical/Question11.java
+++ Lab/Practical/Question11.java
@@ -1,35 +1,31 @@
 
 
-class Shape{
-    
-    float area;
+class Shape {
 
-    void area(int length , int breadth)
-    {
+    double area;
+
+    void area(int length, int breadth) {
         area = length * breadth; // area of rectangle
     }
 
-    void area(float half , int base , int height)
-    {
-        area =  half * base * height; // area of triangle
+    void area(float half, int base, int height) {
+        area = half * base * height; // area of triangle
     }
 
-    void area(int radius)
-    {
-        area = 3.14 * (int)radius * (int)radius; // area of circle
+    void area(int radius) {
+        area = 3.14 * radius * radius; // area of circle
     }
 
-    void display(){
+    void display() {
         System.out.print("Area is = " + area);
+        System.out.println();
     }
 
-
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Shape s = new Shape();
-        s.area(2 , 4);
+        s.area(2, 4);
         s.display();
-        s.area(0.2f , 4 , 6);
+        s.area(0.2f, 4, 6);
         s.display();
         s.area(4);
         s.display();

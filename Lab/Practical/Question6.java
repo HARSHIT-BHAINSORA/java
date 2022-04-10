571c59030aee7fd443efe97cbf893bbd42566eeb
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-04-10 11:07:51 +0530

    String handling

--- Lab/Practical/Question6.java
+++ Lab/Practical/Question6.java
@@ -7,8 +7,27 @@ class Functions{
 
         System.out.println(s.concat("Welcome"));
 
-        // 
+
+        // charAt()
+        System.out.println(s.charAt(1));
 
 
+        // find index at first 'a'
+
+        System.out.println(s.indexOf('a'));
+
+        // find index at second 'a'
+
+        System.out.println(s.lastIndexOf('a')) 
+
+        // Compare 
+
+        System.out.println(s.equals("JAVA"));
+
+        // Compare ignoring case
+        System.out.println(s.equalsIgnore("JAVA"));
+
+        //index of first a from last
+        System.out.println(s.lastIndexOf('a', 2));
     }
 }
